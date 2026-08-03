0a822f9b1067f4ee50ab0e4137133281496aa31c
현대진 <hyeondaejin@hyeondaejin-ui-MacBookPro.local>
2026-08-03 14:34:21 +0900

    fix Param

--- common/params.py
+++ common/params.py
@@ -6,7 +6,7 @@ class Params:
     def __init__(self):
         os.makedirs(PARAMS_DIR, exist_ok=True)
 
-    def get(self, key: str) -> bytes:
+    def get(self, key: str, **kwargs) -> bytes:
         path = os.path.join(PARAMS_DIR, key)
         if not os.path.exists(path):
             return None
@@ -17,7 +17,7 @@ class Params:
         val = self.get(key)
         return val == b"1"
 
-    def put(self, key: str, val: str):
+    def put(self, key: str, val: str, **kwargs):
         path = os.path.join(PARAMS_DIR, key)
         with open(path, "wb") as f:
             f.write(val.encode() if isinstance(val, str) else val)
